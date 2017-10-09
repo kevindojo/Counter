6be9787a0bd8d4d0d976426345553440f3633aac
KevinPham <kevinpham15@gmail.com>
2017-10-09 15:10:15 -0700

    updated with comments

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
-app.secret_key = "22"
+
+app.secret_key = "22"       # secret key needs to be put up here
 
 @app.route('/')
 def index():
-    if 'count' not in session:
+    if 'count' not in session:           # create if and else statement in increment counter
         session['count'] = 0
     else: session['count'] = session['count'] + 1
     return render_template('index.html')
